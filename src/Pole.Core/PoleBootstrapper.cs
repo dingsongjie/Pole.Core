580971a12bc26d99791a731c0b8bd11e77cd7a1d
丁松杰 <377973147@qq.com>
2021-03-05 17:33:16 +0800

    add  PoleBootstrapper

---
+++ src/Pole.Core/PoleBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Core Community. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Pole.Core.Startup;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pole.Core
+{
+    public static class PoleBootstrapper
+    {
+        public static void UsePole(IServiceProvider serviceProvider)
+        {
+            StartupBuilder.StartPole(serviceProvider).GetAwaiter().GetResult();
+        }
+    }
+}
